8cac0dad6b817086a2ab6ff6415a32a6fa94c8f5
humgal <humgal@outlook.com>
2021-01-06 17:21:51 +0800

    rotation

--- src/window_main.c
+++ src/window_main.c
@@ -20,11 +20,27 @@
  */
 #include "awtk.h"
 extern ret_t application_init(void);
+// extern ret_t swtich_rotation_animator(widget_t* win, bool_t start_anim);
+
+ret_t swtich_rotation_animator(widget_t* win, bool_t start_anim) {
+  return_value_if_fail(win != NULL, RET_BAD_PARAMS);
+  widget_t* voice_icon = widget_lookup(win,"voice",TRUE);
+  if(start_anim){
+  widget_create_animator(voice_icon,"rotation(from=0, to=6.28,  duration=2000, repeat_times=0, easing=back_out)");
+
+  }else{
+    widget_stop_animator(voice_icon,"rotation");
+    widget_destroy_animator(voice_icon,"rotation");
+  }
+  return RET_OK;
+
+}
 /**
  * 初始化
  */
 ret_t application_init() {
   widget_t* win = window_open("main");
+  swtich_rotation_animator(win,TRUE);
   return RET_OK;
 }
 
@@ -33,25 +49,4 @@ ret_t application_exit() {
   return RET_OK;
 }
 
-static void time_now_str(char* str, size_t size) {
-  date_time_t dt;
-  date_time_init(&dt);
-  snprintf(str, size, "%04d/%02d/%02d %02d:%02d:%02d", dt.year, dt.month, dt.day, dt.hour,
-           dt.minute, dt.second);
-}
 
-/**
- * 获取并显示系统时间
- */
-static ret_t on_systime_update(const timer_info_t* timer) {
-  widget_t* win = WIDGET(timer->ctx);
-  widget_t* label = widget_lookup(win, "sys_time", TRUE);
-
-  if (label) {
-    char text[64];
-    time_now_str(text, sizeof(text));
-    widget_set_text_utf8(label, text);
-  }
-
-  return RET_REPEAT;
-}
